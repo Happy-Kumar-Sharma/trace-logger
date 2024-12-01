ab6d20e3676032abb08089a0ad8762946ea66ffa
happy kumar sharma <happy96314@gmail.com>
2024-12-01 07:42:19 +0530

    added sensitive filter and mask size

--- tests/test_trace_logger.py
+++
@@ -1,9 +0,0 @@
-from trace_logger.config import get_logger
-from trace_logger.utils import get_trace_id, set_trace_id
-
-
-def test_logger_with_trace_id():
-    set_trace_id("12345")
-    logger = get_logger("test_logger")
-    logger.info("Test log")
-    assert "12345" in get_trace_id()
